b862433c94a0a7409fe1bfc1f1f54aeb3065d5bd
Mathew Cook <cook6348@gmail.com>
2021-09-16 19:19:05 -0500

    Flipping to promise syntax

--- Async-Javascript/async.js
+++ Async-Javascript/async.js
@@ -1,14 +1,14 @@
-document.body.style.backgroundColor = "red";
-setTimeout(() => {
-  document.body.style.backgroundColor = "orange";
-  setTimeout(() => {
-    document.body.style.backgroundColor = "blue";
-    setTimeout(() => {
-      document.body.style.backgroundColor = "teal";
-      setTimeout(() => {
-        document.body.style.backgroundColor = "pink";
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
-console.log("DONE RUNNING");
+// document.body.style.backgroundColor = "red";
+// setTimeout(() => {
+//   document.body.style.backgroundColor = "orange";
+//   setTimeout(() => {
+//     document.body.style.backgroundColor = "blue";
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = "teal";
+//       setTimeout(() => {
+//         document.body.style.backgroundColor = "pink";
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+// console.log("DONE RUNNING");
